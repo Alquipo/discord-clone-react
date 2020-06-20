76cbbdc77b6fad31388c72ca83a0062cf6ffac59
Alquipo <alquiponeto@outlook.com.br>
2020-06-20 16:26:39 -0300

    add Layout

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import {Grid} from './styles'
+//criação padrao de componentes no react
+
+const Layout: React.FC =() =>{
+    return(
+        <Grid>
+
+            
+        </Grid>
+    )
+}
+
+export default Layout
